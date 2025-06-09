bbb2ec6f69c7d83ce3784c6b29020159df58eb12
FAISAL ALTENEIJI <faisal@FAISALs-MacBook-Air.local>
2025-06-09 02:57:56 -0700

    حفظ التعديلات المحلية قبل الدمج

--- lib/screens/social_login_screen.dart
+++ lib/screens/social_login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:io' show Platform;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../screens/landing_screen.dart';
